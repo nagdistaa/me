86862a0b4ea89ee0e0b6acfc2ac0552bcbf6e70a
nagdista <nagdista@gmail.com>
2025-06-17 12:47:36 +0300

    fix fetch

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,19 @@ import React from "react";
 
 async function Home() {
   const currentCountry = "countrycode=EG";
-  console.log(process.env.STATIONS_URL + currentCountry);
+  console.log(process.env.NEXT_PUBLIC_STATIONS_URL + currentCountry);
   const stations = await fetch(process.env.STATIONS_URL + currentCountry).then(
     (res) => res.json()
   );
   return (
     <div className="w-full min-h-[100vh] p-5  md:p-7 lg:p-10 flex justify-center items-center flex-wrap gap-5 md:gap-8">
       {stations.map((station) => (
-        <StationCard key={station.stationuuid} name={station.name} imgSrc={station.favicon} audioSrc={station.url} />
+        <StationCard
+          key={station.stationuuid}
+          name={station.name}
+          imgSrc={station.favicon}
+          audioSrc={station.url}
+        />
       ))}
     </div>
   );
